f36004a5514dc9d438fdb7a994c88e63a7d2b4a9
kosyiev-o <kosyiev.o@gmail.com>
2020-01-29 21:23:24 +0200

    Last version. First task

--- hometask_2/backup.sh
+++ hometask_2/backup.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-#variable with files for backup
-files=$(find $1 -mtime -10) #scan files on 0 level
+#files for backup
+cd $1
+files=$(find . -maxdepth 1 -mtime -1 -not -name .)
 mkdir $2/$(date +%Y%m%d)
-count=0
-buffer=$HOME
 for source in $files
 do
-	if [ $count -gt 0 ]
-	then
-		if [ ../$source != ../$buffer ]
-		then
-			cp -R $source $2/$(date +%Y%m%d) #and we can copy hidden files too
-			count=$count+1
-			buffer = $source
-		fi
-	else
-		cp -R $source $2/$(date +%Y%m%d)
-		count=$count+1
-		buffer=$source
-	fi
+	cp -R $source $2/$(date +%Y%m%d) #and we can copy hidden files too
 done
