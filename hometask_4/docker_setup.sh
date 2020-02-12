16644eb378117c5a001088ba23ff87a9c76b1ba7
kosyiev-o <kosyiev.o@gmail.com>
2020-02-12 16:01:18 +0200

    added code for pushing to dockerhub

--- hometask_4/docker_setup.sh
+++ hometask_4/docker_setup.sh
@@ -16,4 +16,7 @@ apt-get -y install docker-ce docker-ce-cli containerd.io
 cd /home/vagrant/mysql/
 docker build -t mysql_serv .
 docker run --name mysql_serv -d mysql_serv
+docker login --username=kosyiev --password=f18f6908-df0c-4512-8a81-478938d54d6a
+docker tag mysql_serv kosyiev/027:sql
+docker push kosyiev/027:sql
 #for getting access to mysql_server on VM: docker exec -it mysql_serv mysql -uroot -p
